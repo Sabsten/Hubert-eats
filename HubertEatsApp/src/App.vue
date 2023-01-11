a299943fbd22513a887a0099f3bb6b6b0de9eeba
Paul Calimache <paul.calimache@viacesi.fr>
2023-01-11 13:51:20 +0100

    Revert "customeraccountrb"
    This reverts commit ffa27fe84293ea01d9f788c29530c4dcbe7eaeb5.

--- HubertEatsApp/src/App.vue
+++ HubertEatsApp/src/App.vue
@@ -50,10 +50,7 @@ export default defineComponent({
 .container-fluid{
   display: flex;
   flex-direction: column;
-  /* height: calc(100vh - 0px); */
-  
-}
-body {
-  margin:0;
+  height: calc(100vh - 0px);
+  padding: 2rem;
 }
 </style>
